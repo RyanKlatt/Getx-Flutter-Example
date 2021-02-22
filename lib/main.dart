58d7f3c38aba70596b91df46af9e6ced78c5b21d
Ryan Klatt <rpklatt@msn.com>
2021-02-22 07:14:03 -0700

    changed MaterialApp to GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'screens/home_page.dart';
 
 void main() {
@@ -8,8 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Starter',
+    //GetMaterialApp is necessary for routes, snackbars,
+    //internationalization, bottomSheets, dialogs, and high-level apis
+    //related to routes and absence of context.
+    return GetMaterialApp(
+      title: 'GetX Flutter Example',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
