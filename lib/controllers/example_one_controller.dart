3c20b07634278bdcd669c5b875ff57921817cd15
Ryan Klatt <rpklatt@msn.com>
2021-02-23 22:34:06 -0700

    finished example 1

--- lib/controllers/example_one_controller.dart
+++ lib/controllers/example_one_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 class ExampleOneController extends GetxController {
-  var count = 10.obs;
-  increment() => count++;
+  var count = 0.obs;
+  add() => count++;
+  subtract() => count--;
 }
--- lib/screens/example_1.dart
+++ lib/screens/example_1.dart
@@ -12,7 +12,58 @@ class ExampleOnePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('${_exampleOneController.count}'),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                'Wraps counter text with Obx(()=> to observe state changes.',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            IconButton(
+              onPressed: () {
+                _exampleOneController.add();
+              },
+              iconSize: 60.0,
+              icon: Icon(
+                Icons.add_circle,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Obx(
+              () => Text(
+                '${_exampleOneController.count}',
+                style: TextStyle(
+                  fontSize: 32.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            IconButton(
+              onPressed: () {
+                _exampleOneController.subtract();
+              },
+              iconSize: 60.0,
+              icon: Icon(
+                Icons.remove_circle,
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: Text('Go Back'),
+            ),
           ],
         ),
       ),
